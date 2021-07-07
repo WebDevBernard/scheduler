68d967deb61293c825cc7f8f4ef06fc0794cbaf6
bernard <ybernard@gmail.com>
2021-07-07 16:26:53 -0700

    add appointment views and storybook

--- stories/index.js
+++ stories/index.js
@@ -11,8 +11,13 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index"
-import Header from "components/Appointment/Header"
-import Empty from "components/Appointment/Empty"
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm"
+import Status from "components/Appointment/Status"
+import Error from "components/Appointment/Error"
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -138,5 +143,7 @@ storiesOf("InterviewerList", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
-
-  
+  .add("Show ", () => <Show student={"Lynda Miller-Jones"} interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+  .add("Confirm", () => <Confirm message={"Delete the appointment?"} onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+  .add("Status", () => <Status message={"Deleting"} />)
+  .add("Error", () => <Error message={"Could not delete appointment"} onClose={action("onClose")} />)
