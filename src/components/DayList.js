4b5d38886c1459c8366952cdf4da9bfb55bdd2a7
bernard <ybernard@gmail.com>
2021-07-06 22:54:05 -0700

    add props to interviewerlistitem and interviewerlist

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,11 +6,12 @@ export default function DayList(props) {
   const dayList = props.days.map((day) => {
   return (  
     <DayListItem 
+      id={day.id}
       name={day.name} 
       spots={day.spots} 
       selected={day.name === props.day}
       setDay={props.setDay}  
-      key={day.id}/>
+      />
   );
   });
   return <ul>{dayList}</ul>
