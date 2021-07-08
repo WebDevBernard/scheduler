80df7028f0a15700b850e5fb8a511791996f1904
bernard <ybernard@gmail.com>
2021-07-07 19:42:39 -0700

    add state to form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,13 +1,26 @@
-import React from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
-
+import React, { useState } from 'react'
 
 export default function Form(props){
+
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
   return(
 <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
-    <form autoComplete="off">
+    <form onSubmit={event => event.preventDefault()} autoComplete="off">
       <input
         className="appointment__create-input text--semi-bold"
         name={props.name}
@@ -18,11 +31,11 @@ export default function Form(props){
         */
       />
     </form>
-    {/* <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} /> */}
+    <InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
-      <Button danger onClick={props.onCancel}>Cancel</Button>
+      <Button danger onClick={cancel}>Cancel</Button>
       <Button confirm onClick={props.onSave}>Save</Button>
     </section>
   </section>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -6,7 +6,7 @@ export default function InterviewerList(props) {
   const interviewList = props.interviewers.map((interviewer) => {
     return (  
       <InterviewerListItem 
-        id={interviewer.id}
+        key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.interviewer}
