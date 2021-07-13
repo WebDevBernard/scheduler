452259ada54aced5daa41da09e2e16c61dc2ea55
bernard <ybernard@gmail.com>
2021-07-13 02:07:04 -0700

    implement updateSpots remaining

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,68 +2,79 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function useApplicationData(props) {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
 
-// console.log("BEFORE: ", state.days[0].spots)
-  // your code to update the spots goes here
-// console.log("AFTER: ", state.days[0].spots)
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
   
-const [state, setState] = useState({
-  day: "Monday",
-  days: [],
-  appointments: {},
-  interviews: {}
-});
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+      return axios.put(`/api/appointments/${id}`, {interview})
+        .then(() => {
+          setState({
+          ...state,
+          appointments
+          });
+          console.log("BEFORE: ", state.days[0].spots)
+          updateSpots(id)
+          console.log("AFTER: ", state.days[0].spots)
+        })
+    }
+  
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+      }
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+      return axios.delete(`/api/appointments/${id}`)
+        .then(() => {
+          setState({
+          ...state,
+          appointments,
+          });
+          console.log("BEFORE: ", state.days[0].spots)
+          updateSpots(id)
+          console.log("AFTER: ", state.days[0].spots)
+        })
+    }
+  
+  useEffect(() => {
+    Promise.all([
+      axios.get("api/days"),
+      axios.get('api/appointments'),
+      axios.get('api/interviewers')
+    ]).then(axios.spread((response1, response2, response3) => {
+      setState(prev => ({...prev, days: response1.data, appointments: response2.data, interviewers:  response3.data }))
+    }))
+    .catch(error =>
+      console.log(error));
+  }, []);
 
-function bookInterview(id, interview) {
-  const appointment = {
-    ...state.appointments[id],
-    interview: { ...interview }
+  const updateSpots = (id) => {
+    axios.get("/api/days")
+    .then(response => {
+      setState(prev => 
+        ({ ...prev, days: response.data}))
+    })
+    .catch(error =>
+      console.log(error));
+  }
+
+  const setDay = day => setState({ ...state, day });
+
+  return { state, setDay, bookInterview, cancelInterview }
   };
-
-  const appointments = {
-    ...state.appointments,
-    [id]: appointment
-  };
-  return axios.put(`/api/appointments/${id}`, {interview})
-  .then(() => {
-  setState({
-    ...state,
-    appointments
-  });
-})
-}
-
-function cancelInterview(id) {
-const appointment = {
-  ...state.appointments[id],
-  interview: null
-}
-const appointments = {
-  ...state.appointments,
-  [id]: appointment
-};
-return axios.delete(`/api/appointments/${id}`)
-.then(() => {
-  setState({
-    ...state,
-    appointments
-  });
-})
-}
-
-const setDay = day => setState({ ...state, day });
-
-useEffect(() => {
-  Promise.all([
-    axios.get("api/days"),
-    axios.get('api/appointments'),
-    axios.get('api/interviewers')
-  ]).then(axios.spread((response1, response2, response3) => {
-    setState(prev => ({...prev, days: response1.data, appointments: response2.data, interviewers: response3.data }))
-  }))
-  .catch(error =>
-    console.log(error));
-}, []);
-
-return { state, setDay, bookInterview, cancelInterview }
-};
